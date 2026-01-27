1afca92e419c390e8a9ccc3e4e356e191766911f
Phennylalanine <petergregoryj@gmail.com>
2026-01-27 11:19:41 +0900

    Update script.js

--- Lesson7-1/script.js
+++ Lesson7-1/script.js
@@ -204,34 +204,7 @@ window.addEventListener("DOMContentLoaded", () => {
   function saveProgress() {
     localStorage.setItem("lesson7_xp", xp);
     localStorage.setItem("lesson7-1sLevelr", level);
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Quiz Hub - Total Level</title>
-  <link rel="stylesheet" href="style.css" />
-</head>
-<body>
-  <header>
-    <h1>Welcome to the Quiz Hub!</h1>
-    <p id="overallLevel"></p>
-  </header>
-
-  <main>
-    <section class="quiz-grid" id="quizGrid"></section>
-  </main>
-
-  <footer>
-    <p>© 2024–2025 Created by Peter Gregory</p>
-  </footer>
-
-  <script src="js/quizCards.js"></script>
-  <script src="js/monsterHub.js"></script>
-  <script src="js/main.js"></script>
-</body>
-</html>
-", level);
-  }
+   }
 
   function loadProgress() {
     xp = Number(localStorage.getItem("lesson7_xp")) || 0;
